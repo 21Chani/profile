bf37cff30dc98071148d7f665f16ada4cf4650ce
21Chani <dmendoncacosta03@gmail.com>
2023-06-07 18:49:06 -0300

    feat: add "about me" text

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,19 @@ function App() {
         <span className="whitespace-nowrap text-lg text-white font-bold">About me</span>
         <div className="w-full h-0.5 bg-gray-300"></div>
       </div>
-      <div className="w-[400px] h-[300px] bg-gray-300 shadow-xl mt-10 rounded-xl"></div>
+      <p className="p-5 bg-gray-700 rounded-md shadow-[inset_-5px_5px_20px_0px_#0004] text-gray-100 font-semibold mt-4 text-justify">
+        &nbsp;&nbsp;&nbsp;With a strong interest in understanding how things work, I embarked on my
+        programming journey at the age of 15, Through self-guided study, I diligently acquired the
+        skills necessary to construct Minecraft plugins and mods. With a desire to expand my
+        knowledge further,I have since delved into diverse domains, including web development, game
+        design, server administration, mobile application development, and even the intricacies of
+        operating systems.
+        <br /> <br />
+        &nbsp;&nbsp;&nbsp;At the present age of 19, I am actively seeking opportunities to gain
+        valuable market experience and further enhance my skill set. Alongside my passion for
+        programming, I devote a bit of my free time to the art of creating 3D models. Additionally,
+        I maintain a rigorous gym routine, engaging in weightlifting exercises on a daily basis.
+      </p>
     </div>
   )
 }
