bc0e47a481ced55fa0507333637be2f9a149d0df
21Chani <dmendoncacosta03@gmail.com>
2023-06-08 09:12:41 -0300

    feat(config): src & icons aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const srcDir = new URL('./src', import.meta.url).pathname
+const iconsDir = new URL('./src/icons', import.meta.url).pathname
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@src': srcDir,
+      '@icons': iconsDir,
+    },
+  },
 })
